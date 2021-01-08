02252f886677fd4831e247147a4da3bf6ce3116e
Hernán Valdés <hfvaldesg@gmail.com>
2021-01-08 11:53:52 +0100

    Go to normal user for installation of zsh

--- build.sh
+++ build.sh
@@ -17,6 +17,9 @@ code --install-extension azemoh.one-monokai
 code --install-extension k--kato.intellij-idea-keybindings
 
 # Make zsh default shell and change theme (oh-my-zsh)
+# Go back to normal user
+su $SUDO_USER <<EOF
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sed -i 's/robbyrussell/avit/' ~/.zshrc 
+EOF
