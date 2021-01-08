8b7917f38ab53b28d469b41c0c888d39d2f81474
Hernán Valdés <hfvaldesg@gmail.com>
2021-01-08 11:34:05 +0100

    Update dotfiles and add polybar in the installation

--- build.sh
+++ build.sh
@@ -3,19 +3,20 @@
 
 sudo pacman -Syu --noconfirm
 sudo pacman -S --noconfirm rofi firefox hsetroot ksysguard okular gnome-terminal glfw-x11
+sudo pacman -S --noconfirm polybar
 sudo snap install code --classic
 
 # ~/.i3/config
-mv ./.config/i3/config ~/.i3/config
+cp ./config/i3/config ~/.i3/config
+cp ./config/polybar ~/.config
 
 # ~/.config/compton.conf
-mv ./.config/compton/compton.conf ~/.config/compton.conf
+cp ./config/compton/compton.conf ~/.config/compton.conf
 
 # Install VSCode extensions and themes
 code --install-extension ms-vscode.cpptools
 code --install-extension ms-python.python
 code --install-extension azemoh.one-monokai
-code --install-extension ahmadawais.shades-of-purple
 code --install-extension k--kato.intellij-idea-keybindings
 
 # Make zsh default shell and change theme (oh-my-zsh)
