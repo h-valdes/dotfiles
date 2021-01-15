25337d4bad44044894674b7b731a1e22d1f169d7
Hernán Valdés <hfvaldesg@gmail.com>
2021-01-15 10:38:28 +0100

    Update colors on system, use the same as in Xresources

--- fresh-build.sh
+++ fresh-build.sh
@@ -2,7 +2,7 @@
 # Install and config a Manjaro i3 fresh system
 
 sudo pacman -Syu --noconfirm
-sudo pacman -S --noconfirm rofi firefox hsetroot ksysguard okular gnome-terminal glfw-x11
+sudo pacman -S --noconfirm rofi firefox hsetroot ksysguard okular gnome-terminal glfw-x11 cmake
 sudo pacman -S --noconfirm polybar
 sudo snap install code --classic
 
@@ -12,6 +12,8 @@ code --install-extension ms-python.python
 code --install-extension azemoh.one-monokai
 code --install-extension install SolarLiner.linux-themes
 code --install-extension k--kato.intellij-idea-keybindings
+
+mkdir ~/Projects
 
 # Call the script that updates the dotfiles
 sh update.sh
